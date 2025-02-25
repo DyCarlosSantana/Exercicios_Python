7b75133d16ba2b4d4cc1f1ac735526b42176d62d
Edy Carlos de Santana Souza <dy.carlos.santana@gmail.com>
2025-02-24 21:46:09 -0300

    Desafios 41, 43, 44 completos

--- Modulo_02/Aula12_CondiçõesAninhadas/Desafio_43.py
+++ Modulo_02/Aula12_CondiçõesAninhadas/Desafio_43.py
@@ -1,1 +1,26 @@
 #Calcular o IMC de uma pessoa e exibir a faixa de valores...
+idade = int(input('Insira sua Idade: '))
+altura = float(input('Insira sua Altura: '))
+peso = float(input('Insira seu peso: '))
+
+imc = peso / (altura ** 2)
+print('')
+print('-=-'*15)
+print('Sua idade: {}'.format(idade))
+print('Seu Altura: {:.2f}m'.format(altura))
+print('Seu Peso: {:.2f}Kg'.format(peso))
+print('Seu IMC: {:.2f}'.format(imc))
+print('')
+if imc < 18.5:
+    print('Classificação: Abaixo do peso')
+elif 18.5 <= imc < 25:
+    print('Classificação: Peso normal')
+elif 25 <= imc < 30:
+    print('Classificação: Sobrepeso')
+elif 30 <= imc < 35:
+    print('Classificação: Obesidade Grau I')
+elif 35 <= imc < 40:
+    print('Classificação: Obesidade Grau II (severa)')
+else:
+    print('Classificação: Obesidade Grau III (mórbida)')
+print('-=-' * 15)
--- Modulo_02/Aula12_CondiçõesAninhadas/Desafio_44.py
+++ Modulo_02/Aula12_CondiçõesAninhadas/Desafio_44.py
@@ -3,3 +3,35 @@
 # À vista no cartão - 5% de desconto;
 # Em até 2x no cartão - preço normal;
 # 3x no cartão - 20% de juros.
+valor = float(input('Insira o valor do produto: R$'))
+#Formas de pagamento
+print('Formas de pagamento:')
+print('[1] À vista (dinheiro/cheque) - 10% de desconto')
+print('[2] À vista no cartão - 5% de desconto')
+print('[3] Cartão em até 2 vezes - Preço normal')
+print('[4] Cartão em 3 vezes ou mais - 20% de juros')
+
+#Escolha da forma de pagamento
+forma_pagemento = int(input('Forma de pagamento:'))
+a_vista = [1]
+cartao_vista = [2]
+cartao_2x = [3]
+cartao_3x = [4]
+
+#verificação
+if forma_pagemento == 1:
+    valor_final = valor - (valor * 10 /100)
+elif forma_pagemento == 2:
+    valor_final = valor - (valor * 5 /100)
+elif forma_pagemento == 3: 
+    valor_final = valor
+elif forma_pagemento == 4: 
+    valor_final = valor + (valor * 20 /100)
+else:
+    print('Opção Invalida, escolha um némero entre 1 e 4')
+
+#valor final
+print('-=-'*15)
+print('Forma de Pagamento: {}'.format(forma_pagemento))
+print('Valor Total: R${:.2f}'.format(valor_final))
+print('-=-'*15)
