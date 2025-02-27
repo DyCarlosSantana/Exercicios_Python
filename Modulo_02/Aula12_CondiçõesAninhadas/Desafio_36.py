04cec9716ccaf43854b8a32962ba9ecf973115e0
Edy Carlos de Santana Souza <dy.carlos.santana@gmail.com>
2025-02-27 16:43:55 -0300

    Auterações desafio 36

--- Modulo_02/Aula12_CondiçõesAninhadas/Desafio_36.py
+++ Modulo_02/Aula12_CondiçõesAninhadas/Desafio_36.py
@@ -1,17 +1,15 @@
 #Escreva um programa para aprovar o emprestimo bancario para a compra de uma casa. O programa vai perguntar o valor da casa, o salario do comprador, e em quantos anos ele vai pagar.
 #Calcular o valor da prestação mensal, sabendo que ele não pode exceder 30% do salario ou então o emprestimo será negado
-import time
+
 #Coleta dos dados:
-nome = str(input('Digite seu nome: '))
+nome = str(input('Digite seu nome: ')).strip()
 renda_mensal = float(input('Informe sua renda mensal: '))
 valor_imovel = float(input('Informe o valor do imovel: '))
-tempo_pagamento = int(input('Em quantos anos pretende pagar o emprestimo?: '))
+anos = int(input('Em quantos anos pretende pagar o emprestimo?: '))
 print('PROCESSANDO...')
-time.sleep(2)
 
 #Calculo
-qt_meses = tempo_pagamento * 12
-prestacao = valor_imovel / qt_meses
+prestacao = valor_imovel / (anos * 12)
 limite = renda_mensal * 30 /100
 
 #Verificação
