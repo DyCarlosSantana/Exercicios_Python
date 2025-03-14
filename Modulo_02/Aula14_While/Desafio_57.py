ba41bcb632a7cd55748a83db91c9f0723b6b3234
Edy Carlos de Santana Souza <dy.carlos.santana@gmail.com>
2025-03-14 15:24:28 -0300

    Update Desafio_57.py
    Resolvido

--- Modulo_02/Aula14_While/Desafio_57.py
+++ Modulo_02/Aula14_While/Desafio_57.py
@@ -1,1 +1,17 @@
 #Faça um programa que leia o sexo de uma pessoa, mas so aceite os valores 'M' ou 'F'. Caso esteja errado peça a digitação novamente até ter um valor correto.
+
+while True:
+    print('-----'*15)
+    print('Opções: [M]-> Masculino, [F]-> Feminino.')
+    sexo = str(input('Sexo: ')).strip().upper()
+    if sexo in ['F', 'M']:
+        if sexo == 'M':
+            m = 'Masculino'
+            print(f'O sexo escolido foi: {m}')
+        else:
+            f = 'Feminino'
+            print(f'O sexo escolido foi: {f}')
+        break #Interronpe o loop se a resposta for valida
+    else:
+        print('Escolha Invalida! Tente novamente.')
+print('Fim')
