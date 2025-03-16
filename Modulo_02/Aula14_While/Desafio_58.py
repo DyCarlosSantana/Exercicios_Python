04c4897c5c2f99fb2da4dc0789e13f9607c51dda
Edy Carlos de Santana Souza <dy.carlos.santana@gmail.com>
2025-03-15 21:12:36 -0300

    Atualização da resolução

--- Modulo_02/Aula14_While/Desafio_58.py
+++ Modulo_02/Aula14_While/Desafio_58.py
@@ -1,24 +1,19 @@
 #Melhore o jogo do desafio 29 onde o computador vai "Pensar" em um núemro de 0 a 10. Só que agora o jogador vai tentra adivinhar até acertar, mostrando no final quantos palpites foram necessarios para vencer.
 import random
-import time
-n = random.randint(0, 11)
-print('-=-'*20)
+print('---'*20)
 print('Vou pensar em um número entre 0 e 10. Tente adivinhar...')
-print('-=-'*20)
+computador = random.randint(0, 11)
 palpites = 0
-while True:
+acertou = False
+while not acertou: #Enquanto não for verdadeiro...
     jogador = int(input('Faça seu chute: '))
-    print('PROCESSANDO...')
-    time.sleep(1)
     palpites += 1
-    if jogador == n:
-        print('-'*25)
-        print('Pensei no número {}'.format(n))
-        if palpites == 1:
-            print(f'Você acertou na {palpites}° tentativa')
-        else:
-            print(f'Você precisou de {palpites} tentativas')
-
-        break #interronpe o loop caso o valor seja valido
+    if jogador == computador:
+        acertou = True 
     else:
-        print('Errou! Mas você pode tente novamente')
+        if jogador < computador:
+            print(f'Mais... Tente novamente')
+        elif jogador > computador:
+            print(f'Menos... Tente novamente')
+print(f'Você acertou com {palpites} tentativa(s)')
+print('---'*20)
