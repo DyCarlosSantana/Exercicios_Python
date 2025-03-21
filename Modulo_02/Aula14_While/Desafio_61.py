0ae3ef432c1870cf83ce22594c9f7b85974eb828
Edy Carlos de Santana Souza <dy.carlos.santana@gmail.com>
2025-03-21 13:49:43 -0300

    Desafios 61 e 65

--- Modulo_02/Aula14_While/Desafio_61.py
+++ Modulo_02/Aula14_While/Desafio_61.py
@@ -1,1 +1,12 @@
-#Refaça o desafio 51, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da profressão usando a estrutura while.
+#Refaça o desafio 51, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão usando a estrutura while.
+print('='*20)
+print('10 PRIMEIROS TERMOS')
+print('='*20)
+
+primeiro_termo = int(input('Primeiro termo: '))
+razao = int(input('Razão: '))
+decimo = primeiro_termo + (10 - 1) * razao #Formula para o Decimo termo de uma PA
+
+for c in range(primeiro_termo, decimo + razao, razao):
+    print('{}'.format(c),end=' -> ')
+print('Acabou')
--- Modulo_02/Aula14_While/Desafio_65.py
+++ Modulo_02/Aula14_While/Desafio_65.py
@@ -1,1 +1,9 @@
 #crie um programa que leia varios numeros inteiros pelo teclado, no final da execução mostre a média entre todos os valores e quais foram o maior e menor valores lidos. O programa deve perguntar aos usuario de quer ou não continuar a digitra valores.
+cont = 0
+while True:
+    num = int(input('Digite um valor: '))
+    cont += 1
+    while True:
+        media += media / cont
+        print(media)
+    
