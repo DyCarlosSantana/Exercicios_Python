92a8b33b94aa42ab81ed44e10e12b7638743c36b
Edy Carlos de Santana Souza <dy.carlos.santana@gmail.com>
2025-03-24 15:19:42 -0300

    Update Desafio_65.py
    Resolução

--- Modulo_02/Aula14_While/Desafio_65.py
+++ Modulo_02/Aula14_While/Desafio_65.py
@@ -1,17 +1,25 @@
 #crie um programa que leia varios numeros inteiros pelo teclado, no final da execução mostre a média entre todos os valores e quais foram o maior e menor valores lidos. O programa deve perguntar aos usuario de quer ou não continuar a digitra valores.
 cont = 0
 total = 0
-print('Para encerra o programa digite "000"')
+maior = 0
+menor = 0
 while True:
     num = int(input('Digite um número: '))
+    resp = str(input('Gostaria de continuar?: [S/N] ')).upper().strip()
     total += num
     cont += 1
-    if num == 000:
-        cont -= 1
+    if cont == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        elif num < menor:
+            menor = num
+    if resp == 'N':
         break
     print('-----'*5)
     
-print(f'{cont}, {total}')
 media = total / cont
 print(f'A média de todos os valores inseridos é de: {media}')
+print('O maior valor foi {maior}, e o menor valor foi {menor}') 
 print('Programa encerrado!')
