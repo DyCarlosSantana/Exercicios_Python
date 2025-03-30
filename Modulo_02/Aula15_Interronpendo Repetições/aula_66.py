c9b1fba215efb28d675f18cebbc6fa15b84d1eaf
Edy Carlos de Santana Souza <dy.carlos.santana@gmail.com>
2025-03-30 13:06:31 -0300

    Update aula_66.py
    Resolução propria

--- Modulo_02/Aula15_Interronpendo Repetições/aula_66.py
+++ Modulo_02/Aula15_Interronpendo Repetições/aula_66.py
@@ -5,3 +5,15 @@
 # No final, mostrar:
 # Quantos números foram digitados.
 # A soma entre eles (desconsiderando o 999).
+cont = 0
+soma = 0
+print('Condição de parada [999]')
+while True:
+    num = int(input('Digite um número: '))
+    cont += 1
+    soma += num
+    if num == 999:
+        cont -=1
+        soma -= 999
+        break
+print(f'Forma digitados {cont} números.\n A soma deles é: {soma}')
