69651ce2d6f1ba4aadfea7835f8b7c84bc7ed460
Edy Carlos de Santana Souza <dy.carlos.santana@gmail.com>
2025-04-13 16:14:46 -0300

    Update aula_71.py

--- Modulo_02/Aula15_Interronpendo Repetições/aula_71.py
+++ Modulo_02/Aula15_Interronpendo Repetições/aula_71.py
@@ -3,3 +3,7 @@
 # Simular um caixa eletrônico que pergunta o valor a ser sacado (número inteiro).
 # O programa deve informar quantas cédulas de cada valor serão entregues.
 # Cédulas disponíveis: R$50,0 R$20,0 R$10,0 R$1,0
+
+print('\n' + '-' * 15, 'CAIXA ELETRÔNICO', '-' * 15)
+valor_saque = int(input('Digite o valor para saque:\nR$ '))
+
