75a5aa9263222cdb5131264d7d253f34a5f90955
Edy Carlos de Santana Souza <dy.carlos.santana@gmail.com>
2025-04-13 18:23:58 -0300

    Update aula_71.py
    Resolução + correção e melhoria

--- Modulo_02/Aula15_Interronpendo Repetições/aula_71.py
+++ Modulo_02/Aula15_Interronpendo Repetições/aula_71.py
@@ -4,6 +4,32 @@
 # O programa deve informar quantas cédulas de cada valor serão entregues.
 # Cédulas disponíveis: R$50,0 R$20,0 R$10,0 R$1,0
 
-print('\n' + '-' * 15, 'CAIXA ELETRÔNICO', '-' * 15)
-valor_saque = int(input('Digite o valor para saque:\nR$ '))
-
+print('=' * 30)
+print('{:^30}'.format('BANCO CEV'))
+print('=' * 30)
+valor = int(input('Valor para saque: R$ '))
+total = valor
+cedula = 100
+total_cedulas = 0
+while True:
+    if total >= cedula:
+        total -= cedula
+        total += 1
+    else:
+        if total_cedulas > 0:
+            print(f'Total de {total_cedulas} cédulas de R${cedula}')
+        if cedula == 100:
+            cedula = 50
+        elif cedula == 50:
+            cedula = 20
+        elif cedula == 20:
+            cedula = 10
+        elif cedula == 10:
+            cedula = 5
+        elif cedula == 5:
+            cedila = 1
+        total_cedulas = 0
+        if total == 0:
+            break
+print('=' * 30)
+print('Volte senpre ao Banco CEV!')
